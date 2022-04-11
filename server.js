b3024ac104499ecf15d1e06821cbb451f572daf1
jahnhah <jahnhahcraven@gmail.com>
2022-04-12 01:44:46 +0300

    config

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(requireHTTPS);
-app.use(express.static('./dist/front'));
-app.get('/', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
-  );
+app.use(express.static(__dirname + '/dist/i-kali'));
+
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/i-kali/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+
+
+app.listen(process.env.PORT || 80);
